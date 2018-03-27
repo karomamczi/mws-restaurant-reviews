7b3c9b362cb56da870db3c31f699c0535fb40519
Karolina Mamczarz <karolina.mamczarz@op.pl>
2018-03-27 19:50:04 +0200

    feat: Simple fixes to templates, automatic year updater added

---
+++ js/current_year.js
@@ -0,0 +1,16 @@
+/**
+ * Fetch current year as soon as the page is loaded.
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+  getCurrentYear();
+
+});
+
+/**
+ * Get current year for the footer.
+ */
+getCurrentYear = () => {
+  const year = document.getElementById('year');
+  const currentYear = new Date().getFullYear();
+  year.innerHTML = currentYear;
+};
