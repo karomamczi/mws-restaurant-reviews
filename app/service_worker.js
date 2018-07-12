e626759ec4234e162c6a884445a4ffe1667f0b51
Karolina Mamczarz <karolina.mamczarz@op.pl>
2018-07-12 22:07:35 +0200

    refactor: Changed name of callback

--- app/service_worker.js
+++ app/service_worker.js
@@ -41,12 +41,11 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.open('restaurants').then((cache) => {
       return cache.match(event.request).then((response) => {
-        return response || fetch(event.request).then((res) => {
-          cache.put(event.request, res.clone());
-          return res;
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
         });
       })
     })
-
   );
 });
