044b40724d8593d4fb897faaf8adb1fd3668d24c
Karolina Mamczarz <karolina.mamczarz@op.pl>
2018-07-15 08:57:37 +0200

    refactor: Added catch and else to service worker registration method

--- app/js/service_worker_register.js
+++ app/js/service_worker_register.js
@@ -1,7 +1,10 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service_worker.js').then((registration) =>
-      console.log('ServiceWorker registration successful with scope: ', registration.scope),
-      (err) => console.log('ServiceWorker registration failed: ', err));
+    navigator.serviceWorker.register('service_worker.js')
+      .then((registration) =>
+        console.log('ServiceWorker registration successful with scope: ', registration.scope)
+      .catch((err) => console.log('ServiceWorker registration failed: ', err)));
   });
+} else {
+  console.log('Service Worker is not supported in this. browser.');
 }
