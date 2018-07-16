b056199139e1b9e8bd8f9f6de2ae342ae6a1a823
Karolina Mamczarz <karolina.mamczarz@op.pl>
2018-07-16 20:22:25 +0200

    chore: fetching data

--- app/service_worker.js
+++ app/service_worker.js
@@ -1,77 +1,83 @@
-// const cacheVersion = 'restaurant-reviews';
+const cacheVersion = 'restaurant-reviews';
 
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     caches.open(cacheVersion).then((cache) => {
-//       return cache.addAll([
-//         '/',
-//         '/index.html',
-//         '/restaurant.html',
-//         '/manifest.json',
-//         '/js/restaurants.js',
-//         '/js/restaurant_info.js',
-//         '/js/dbhelper.js',
-//         '/js/current_year.js',
-//         '/js/restaurants_db.js',
-//         '/js/service_worker_register.js',
-//         '/css/styles.css',
-//         '/img/1.jpg',
-//         '/img/2.jpg',
-//         '/img/3.jpg',
-//         '/img/4.jpg',
-//         '/img/5.jpg',
-//         '/img/6.jpg',
-//         '/img/7.jpg',
-//         '/img/8.jpg',
-//         '/img/9.jpg',
-//         '/img/10.jpg',
-//         '/img/restaurant-16.png',
-//         '/img/restaurant-128.png',
-//         '/img/restaurant-256.png',
-//         '/img/restaurant-512.png'
-//         ]).then(() => {
-//           console.log('All files are cached successfully.');
-//           return self.skipWaiting();
-//         }).catch((err) => {
-//           console.log('File cache failed with:', err)
-//         });
-//     })
-//   );
-// });
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheVersion).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/manifest.json',
+        '/js/restaurants.js',
+        '/js/restaurant_info.js',
+        '/js/dbhelper.js',
+        '/js/current_year.js',
+        '/js/restaurants_db.js',
+        '/js/service_worker_register.js',
+        '/css/styles.css',
+        '/img/1_400.jpg',
+        '/img/1_600.jpg',
+        '/img/2_400.jpg',
+        '/img/2_600.jpg',
+        '/img/3_400.jpg',
+        '/img/3_600.jpg',
+        '/img/4_400.jpg',
+        '/img/4_600.jpg',
+        '/img/5_400.jpg',
+        '/img/5_600.jpg',
+        '/img/6_400.jpg',
+        '/img/6_600.jpg',
+        '/img/7_400.jpg',
+        '/img/7_600.jpg',
+        '/img/8_400.jpg',
+        '/img/8_600.jpg',
+        '/img/9_400.jpg',
+        '/img/9_600.jpg',
+        '/img/10_400.jpg',
+        '/img/10_600.jpg',
+        '/img/restaurant-16.png',
+        '/img/restaurant-128.png',
+        '/img/restaurant-256.png',
+        '/img/restaurant-512.png'
+        ]).then(() => {
+          console.log('All files are cached successfully.');
+          return self.skipWaiting();
+        }).catch((err) => {
+          console.log('File cache failed with:', err)
+        });
+    })
+  );
+});
 
-// self.addEventListener('activate', (event) => {
-//   event.waitUntil(
-//     caches.keys().then((keys) => {
-//       return Promise.all(keys.map((key, i) => {
-//         if (key !== cacheVersion) {
-//           return caches.delete(keys[i]);
-//         }
-//       }));
-//     })
-//   )
-// });
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [cacheVersion];
 
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       if (response) {
-//         return response;
-//       }
-//       requestBackend(event);
-//     })
-//   )
-// });
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
 
-// const requestBackend = (event) => {
-//   const url = event.request.clone();
-//   return fetch(url, {mode: 'no-cors'}).then((response) => {
-//     if (!response || response.status !== 200 || response.type !== 'basic') {
-//       return response;
-//     }
-//     const responseToCache = response.clone();
-//     caches.open(cacheVersion).then((cache) => {
-//       cache.put(event.request, responseToCache);
-//     });
-//     return response;
-//   })
-// };
+
+self.addEventListener('fetch', (event) => {
+  console.log('url', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+        .then(response => {
+          if (response.status === 200) {
+            console.log(response.status)
+            const responseToCache = response.clone();
+            caches.open(cacheVersion).then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+          }
+        });
+    })
+  )
+});
